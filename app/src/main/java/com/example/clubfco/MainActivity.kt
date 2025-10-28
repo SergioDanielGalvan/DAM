29edb293bdf2f2614608dc4d0d1338bc08d77a5b
Sergio Daniel Galván <sdg@reportsystem.com.ar>
2025-10-27 22:38:10 -0300

    Codigo de txt y botones en activity_main.xml

--- app/src/main/java/com/example/clubfco/MainActivity.kt
+++ app/src/main/java/com/example/clubfco/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.clubfco
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +17,23 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val txtUsuario : EditText = findViewById<EditText>( R.id.txtUsuario)
+        val txtContraseña : EditText = findViewById<EditText>( R.id.txtContraseña)
+        val btnCancelar : Button = findViewById<Button>( R.id.btnCancelar)
+        btnCancelar.setOnClickListener{
+            Toast.makeText( this, "Boton Cancelar", Toast.LENGTH_SHORT ).show()
+            // Pongo vacias a txtUsuario y txtContraseña
+            // y el foco en txtContraseña
+            txtContraseña.setText("")
+            txtUsuario.setText("")
+            txtUsuario.requestFocus()
+        }
 
-
+        val btnIniciarSesion : Button = findViewById<Button>( R.id.btnIniciarSesion)
+        btnCancelar.setOnClickListener{
+            Toast.makeText( this, "Inicio Ok", Toast.LENGTH_SHORT ).show()
+            // Verificar credenciales
+            // Enviar a activity menu
         }
     }
+}
