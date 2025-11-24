d45bb4b90ea36f39cda468de4c172ae936d0e600
Sergio Daniel Galván <sdg@reportsystem.com.ar>
2025-11-24 14:21:36 -0300

    Correciones de Navegación

--- app/src/main/java/com/example/clubfco/MenuPrincipalActivity.kt
+++ app/src/main/java/com/example/clubfco/MenuPrincipalActivity.kt
@@ -1,5 +1,6 @@
 package com.example.clubfco
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -11,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.appcompat.app.AlertDialog
 
 class MenuPrincipalActivity : AppCompatActivity() {
+    @SuppressLint("StringFormatInvalid")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,39 +23,38 @@ class MenuPrincipalActivity : AppCompatActivity() {
             insets
         }
 
-        val usuario = intent.getStringExtra("Usuario") ?: "usuario"
         val btnSalir = findViewById<Button>(R.id.btnSalir)
         val btnSocios = findViewById<Button>(R.id.btnSocios)
         val btnNoSocios = findViewById<Button>(R.id.btnNoSocios)
-        val btnListarMorosos = findViewById<Button>(R.id.btnListaVenceHoy)
+        //val btnListarMorosos = findViewById<Button>(R.id.btnListaVenceHoy)
 
-            btnSocios.setOnClickListener {
-                val intent = Intent(this, SociosActivity::class.java)
+        btnSocios.setOnClickListener {
+            val intent = Intent(this, SociosActivity::class.java)
+            startActivity(intent)
+
+        }
+
+        btnNoSocios.setOnClickListener {
+            val intent = Intent(this, PagoNoSociosActivity::class.java)
+            startActivity(intent)
+        }
+
+        val nombreUsuario = intent.getStringExtra("Usuario") ?: "usuario"
+        val txtUsuario = findViewById<TextView>(R.id.Usuario)
+        txtUsuario.text = "Bienvenido: $nombreUsuario"
+        //txtUsuario.text = getString(R.string.bienvenido_usuario, nombreUsuario )
+        btnSalir.setOnClickListener {
+            AlertDialog.Builder(this)
+            .setTitle("Cerrar Sesión")
+            .setMessage("¿Estás seguro de que deseas salir?")
+                .setPositiveButton("Si") { _, _ ->
+                //finishAffinity()
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
-
             }
+            .setNegativeButton("No", null)
+            .show()
 
-            btnNoSocios.setOnClickListener {
-                val intent = Intent(this, PagoNoSociosActivity::class.java)
-                startActivity(intent)
-            }
-
-
-            val nombreUsuario = intent.getStringExtra("Usuario") ?: "usuario"
-            val txtUsuario = findViewById<TextView>(R.id.Usuario)
-            txtUsuario.text = "Bienvenido, $nombreUsuario"
-
-
-            btnSalir.setOnClickListener {
-                AlertDialog.Builder(this)
-                .setTitle("Cerrar Sesión")
-                .setMessage("¿Estás seguro de que deseas salir?")
-                    .setPositiveButton("Si") { _, _ ->
-                    finishAffinity()
-                }
-                .setNegativeButton("No", null)
-                .show()
-
-            }
         }
     }
+}
